74fc367c4be01ce92e13925e39a6649e81fb5ce7
Lucas Brito <lucasbnunes23@gmail.com>
2024-12-31 13:26:10 -0300

    feat: add dynamic route to fetch products

---
+++ src/app/shop/[category]/page.tsx
@@ -0,0 +1,13 @@
+import { getProductsByCategory } from '@/features/products/api/get-products';
+import { ProductsGrid } from '@/features/products/products-grid';
+
+export default async function ProductsPage({
+  params,
+}: {
+  params: Promise<{ category: string }>;
+}) {
+  const category = (await params).category;
+  const response = await getProductsByCategory(category);
+
+  return <ProductsGrid products={response.products} />;
+}
--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,8 +1,3 @@
-import { getProductsByCategory } from '@/features/products/api/get-products';
-import { ProductsGrid } from '@/features/products/products-grid';
-
 export default async function ShopPage() {
-  const response = await getProductsByCategory('mens-shoes');
-
-  return <ProductsGrid products={response.products} />;
+  return <>In progress...</>;
 }

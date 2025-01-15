6864fa657bd5c31e4eea46a1bbed75c7a6fa21f7
Lucas Brito <lucasbnunes23@gmail.com>
2025-01-14 22:55:41 -0300

    feat: add callback to close the navigation when clicked

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import { ChevronDown } from 'lucide-react';
 import { Button } from './ui/button';
 import { Popover, PopoverContent, PopoverTrigger } from './ui/popover';
+import React from 'react';
 
 type Category = {
   [key: string]: { title: string; href: string }[];
@@ -55,15 +56,23 @@ export function AppNavigation() {
       <ul className="flex gap-4">
         {Object.keys(categories).map((category) => (
           <li key={category}>
-            <Popover>
-              <Trigger>{category}</Trigger>
-
-              <Content items={categories[category]} />
-            </Popover>
+            <CustomPopover category={category} />
           </li>
         ))}
       </ul>
     </nav>
+  );
+}
+
+function CustomPopover({ category }: { category: string }) {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  return (
+    <Popover open={isOpen} onOpenChange={setIsOpen}>
+      <Trigger>{category}</Trigger>
+
+      <Content items={categories[category]} onClick={() => setIsOpen(false)} />
+    </Popover>
   );
 }
 
@@ -81,13 +90,24 @@ function Trigger({ children }: { children: string }) {
   );
 }
 
-function Content({ items }: { items: { title: string; href: string }[] }) {
+function Content({
+  items,
+  onClick,
+}: {
+  items: { title: string; href: string }[];
+  onClick: () => void;
+}) {
   return (
     <PopoverContent collisionPadding={16}>
       <ul>
         {items.map((item) => (
           <li key={item.title}>
-            <Button asChild variant="ghost" className="w-full justify-start">
+            <Button
+              asChild
+              variant="ghost"
+              className="w-full justify-start"
+              onClick={onClick}
+            >
               <Link href={item.href}>{item.title}</Link>
             </Button>
           </li>
